c69354cb9612be88b10c02464cc1c5599b8a91db
Erick Junior <erickjuniorbatistagarcia2@gmail.com>
2024-03-30 00:52:57 -0300

    avançamos no javascript

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 70
+console.log(`A velocidade do seu carrro é ${vel}Km/h`)
+
+if (vel > 60) { //Condição Simples! 
+    console.log(`Você ultrapassou a velocidade permitida.\nMultado!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança!`)
