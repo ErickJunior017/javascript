4d82f6bbd91b51db8643a8e78b9adf93dae63692
Erick Junior <erickjuniorbatistagarcia2@gmail.com>
2024-03-31 22:16:10 -0300

    termino de repetições em javascript

--- aula14ex/ex016/conta.js
+++ aula14ex/ex016/conta.js
@@ -1,16 +1,31 @@
-
-
-
 function contar() {
-    var tinicio = document.getElementById('txtnumIn')
-var tfim = document.getElementById('txtnumFim')
-var tpasso = document.getElementById('txtpasso')
-var inicio = Number(tinicio.value)
-var fim = Number(tfim.value)
-var passo = Number(tpasso.value)
-    while(inicio <= fim){
-        document.write('passo')
-        inicio = inicio + passo
-    }
+    let iinicio = document.getElementById('txtinicio')
+    let ifim    = document.getElementById('txtfim')
+    let ipasso  = document.getElementById('txtpasso')
+    let res     = document.getElementById('res')
 
-}
+    if (iinicio.value.length == 0 || ifim.value.length == 0 || ipasso.value.length == 0){
+        res.innerHTML = 'Impossível contar!'
+    } else{
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(iinicio.value)
+        let f = Number(ifim.value)
+        let p = Number(ipasso.value)
+        if ( p <= 0){
+            alert('Passo invalido (Somente numeros naturais maior que 0). Considerando passo = 1')
+            p = 1
+        }
+        if ( i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `\u{1F449} ${c}`
+            }
+            
+        } else {
+            for(c = i; c >= f; c -= p){
+                res.innerHTML += `\u{1F449} ${c}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`    
+        
+    }
+} 
